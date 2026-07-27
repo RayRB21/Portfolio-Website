966253e6aa10aab0b294df1bd368aca0acdae6c0
Rayan Butt <rayrb2001@gmail.com>
2026-07-27 13:12:51 +0100

    updated with new project

--- Portfolio/src/pages/Projects.tsx
+++ Portfolio/src/pages/Projects.tsx
@@ -27,19 +27,19 @@ function Projects() {
           appLink="https://github.com/RayRB21/tcg-price-tracker"
         />
         <ProjectTemplate
+          projectName="Sour Casa Bakery E-Commerce Website (React /Typescript /Flask)"
+          projectDesc="Built and deployed a full-stack e-commerce website for a family bakery using React, TypeScript, and Flask. Implemented a responsive user interface, dynamic product catalogue, shopping basket, Stripe payment integration, and delivery scheduling logic. Currently expanding the backend with a PostgreSQL database to replace hardcoded product data, enabling persistent product, order, and customer management through a REST API."
+          projectImg="/website-image.png"
+          projectLink="https://github.com/RayRB21/SourCasa-Website"
+          appLink="https://sourcasa.netlify.app/"
+        />
+        <ProjectTemplate
           projectName="Budgeting Web App (Flask)"
           projectDesc="Developed, deployed and maintained a personal budgeting web application which uses a linked database, enabling users to record income and expenses, set savings goals, and view spending trends. The project incorporates Python, HTML/CSS, and Postgres for backend, frontend and data storage, demonstrating skills in full-stack development, API design, and database integration. It also incorporates a CI/CD Pipeline with GitHub Actions to automate testing and improve deployment reliability.
 "
           projectImg="/budgeter-graphs.png"
           projectLink="https://github.com/RayRB21/Budgeter"
           appLink="https://budgeterapp-e0fa271274bb.herokuapp.com/"
-        />
-        <ProjectTemplate
-          projectName="Portfolio Website"
-          projectDesc="Built and deployed a responsive personal portfolio website using React (TypeScript) and Bootstrap, featuring animated UI components, scroll-triggered effects, and reusable architecture, hosted on Netlify with continuous deployment via GitHub."
-          projectImg="/website-image.png"
-          projectLink="https://github.com/RayRB21/Portfolio-Website"
-          appLink="#"
         />
       </div>
     </div>
